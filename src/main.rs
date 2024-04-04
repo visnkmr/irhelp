3065d55d5f12d3fb4214e3dfdaa45d504447c432
12core1 <12533753+visnkmr@users.noreply.github.com>
2024-04-04 16:34:42 +0530

    sort by distance and save for testing only

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+#[derive(Clone,Serialize,Deserialize,Debug)]
+struct statdist{
+    name:String,
+    distance:f64
+}
 fn main() {
-    fs::write("../test.txt", format!("{:?}","vecofdist")).unwrap();
 
     let fileread=fs::read_to_string("../export.geojson").unwrap();
     // println!("{:?}",fileread.lines().count());
@@ -30,36 +34,48 @@ fn main() {
     //     println!("{:?}",vecofgcoord[i]);
     // }
     let mut vecofdist= Vec::new();
-    let i=vecofgcoord[0].clone();
+    let found:Vec<usize>=vecofstationnames.iter().enumerate().filter_map(|(l,f)|if(f.to_lowercase().contains("")){Some(l)}else{None}).collect();
+    println!("{:?}",found);
+    let i=vecofgcoord[found[0]].clone();
     // for i in vecofgcoord.clone()
      {
         let source=Location::new(i[0],i[1]);
-        for j in vecofgcoord.clone() {
+        for (index,j) in vecofgcoord.clone().iter().enumerate() {
             let dest=Location::new(j[0],j[1]);
             let distance = source.haversine_distance_to(&dest);
-            vecofdist.push((distance.meters()*0.001).round().to_string())
+            vecofdist.push(statdist{
+                name:format!("from {} to {}",vecofstationnames[found[0]],vecofstationnames[index]),
+                distance:(distance.meters()*0.001).round()
+            })
         }
     }
+    vecofdist.sort_by(|a,b|if(a.distance>b.distance){
+        Ordering::Greater
+    }
+    else{
+        Ordering::Less
+    }
+);
     let mut sb=String::new();
     for i in vecofdist.clone(){
-        sb=(format!("{} , {}",sb,i))
+        sb=(format!("{} , {}",sb,serde_json::to_string(&i).unwrap()))
     }
     println!("{:?}",sb.len());
     println!("{:?}",vecofdist.len());
     let mut f = File::options().write(true).append(true).create(true).open("../test.txt").unwrap();
     let mut lw=LineWriter::new(f);
-    fs::write("../test.txt", serde_json::to_string(&vecofdist).unwrap());
-    // for (index,i) in (vecofdist.iter().enumerate()){
-    //     lw.write_all(&format!("{}",i).as_bytes());
-    //     if(index%vecofdist.len()==0){
+    // fs::write("../test.txt", serde_json::to_string(&vecofdist).unwrap());
+    for (index,i) in (vecofdist.iter().enumerate()){
+        lw.write_all(&format!("{}",serde_json::to_string(&i).unwrap()).as_bytes());
+        // if(index%vecofdist.len()==0){
 
-    //         lw.write_all(b"\n");
-    //     }
-    //     else{
-    //         lw.write_all(b" , ");
+            lw.write_all(b"\n");
+        // }
+        // else{
+        //     lw.write_all(b" , ");
 
-    //     }
-    // }
+        // }
+    }
     // fs::write("../test.txt", format!("{:?}",vecofdist)).unwrap();
     // let result: Vec<String> = vecofstationnames.iter().flat_map(|x| vec![x.clone(); vecofstationnames.len()]).collect();
     // vecofgtypes.sort();
@@ -69,7 +85,7 @@ fn main() {
 
 }
 use core::time;
-use std::{fs::{self, File}, io::{LineWriter, Write}, vec};
+use std::{cmp::Ordering, fs::{self, File}, io::{LineWriter, Write}, vec};
 
 use geoutils::Location;
 use serde::{de, Deserialize, Serialize};
