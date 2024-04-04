ea0d7782b8ff760c2024a8bda0b8e4f98e61a4ff
12core1 <12533753+visnkmr@users.noreply.github.com>
2024-04-04 14:19:00 +0530

    calc haversine and save

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,59 @@
 fn main() {
+    fs::write("../test.txt", format!("{:?}","vecofdist")).unwrap();
+
     let fileread=fs::read_to_string("../export.geojson").unwrap();
-    println!("{:?}",fileread.lines().count());
+    // println!("{:?}",fileread.lines().count());
     let data = &fileread;
     
     let feature_collection: FeatureCollection = serde_json::from_str(data).unwrap();
     let listofstations=feature_collection.features;
     let mut vecofgcoord=Vec::new();
     let mut vecofgtypes=Vec::new();
+    let mut vecofstationnames=Vec::new();
     for i in listofstations{
-        vecofgcoord.push(i.geometry.coordinates);
-        if let Some(gtype)= (i.geometry.type_){
-
-            vecofgtypes.push(gtype)
+        if(i.geometry.type_.is_some() && i.properties.name.is_some()){
+            if let Some(gtype)= (i.geometry.type_){
+    
+                vecofgtypes.push(gtype)
+            }
+            if let Some(sname)= (i.properties.name){
+    
+                vecofstationnames.push(sname)
+            }
+            vecofgcoord.push(i.geometry.coordinates);
+        }
+        else{
+            // println!("{:?}",(i.properties));
         }
     }
+    // for i in 0..vecofstationnames.len()-1{
+    //     println!("{:?}",vecofgcoord[i]);
+    // }
+    let mut vecofdist= Vec::new();
+    let i=vecofgcoord[0].clone();
+    for i in vecofgcoord.clone()
+     {
+        let source=Location::new(i[0],i[1]);
+        for j in vecofgcoord.clone() {
+            let dest=Location::new(j[0],j[1]);
+            let distance = source.haversine_distance_to(&dest);
+            vecofdist.push(distance.meters()*0.001)
+        }
+    }
+    println!("{:?}",vecofdist.len());
+    fs::write("../test.txt", format!("{:?}",vecofdist)).unwrap();
+    // let result: Vec<String> = vecofstationnames.iter().flat_map(|x| vec![x.clone(); vecofstationnames.len()]).collect();
     // vecofgtypes.sort();
-    println!("{:#?} {:?}", vecofgcoord.len(),vecofgtypes.len());
     vecofgtypes.dedup();
-    println!("{:#?} {:?}", vecofgcoord.len(),vecofgtypes.len());
+    println!("{:#?} {:?} {:?}", vecofgcoord.len(), vecofstationnames.len(),vecofgtypes.len());
+    // println!("{:#?}", result.len());
 
 }
-use std::fs;
+use core::time;
+use std::{fs, vec};
 
-use serde::{Deserialize, Serialize};
+use geoutils::Location;
+use serde::{de, Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct FeatureCollection {
